3fde30a8045fc95f4fc80c364da9ac996bb01e50
Andrew Gruner <andrew@divineflame.com>
2011-12-19 17:05:54 -0800

    updating js

--- src/BibleWorm/MobileBundle/Resources/public/js/readingplan.js
+++ src/BibleWorm/MobileBundle/Resources/public/js/readingplan.js
@@ -1,69 +1,16 @@
 $(function(){
-    
-    window.Reading = Backbone.Model.extend({
-    });
-    
-    window.ReadingList = Backbone.Collection.extend({
-        model: Reading,
-        
-        url: function() {
-            return 'app_dev.php/api/v1/reading-plan';
-        }
-    });
-    
-    window.Readings = new JotList;
-    
-    window.ReadingView = Backbone.View.extend({
 
-        tagName: "div",
-        //className: "jot",
-        //template: _.template($('#jot_template').html()),
-        events: {
-            "click .ui-checkbox"  : "read"
-        },
+    var $readings = $(.ui-checkbox);
+    
+    $readings.click(function() {
+        var $reading = $(this),
+            $parent = $reading.parents('.ui-collapsible'),
+            $siblings = $reading.siblings();
         
-        initialize: function() {
-            this.model.bind('change', this.render, this);
-        },
-
-        render: function() {
-            this.$el = $(this.el);
-            //this.$el.html(this.template(this.model.toJSON()));
-            
-            return this;
-        },
+        // open reading
         
-        read: function() {
-            // if all three checked, change style of list to read
-            
-            // load reading in new pane
-        },
-
+        // mark set as read if all siblings are checked
         
-        save: function() {
-        }
+        // save state
     });
-    
-    
-    window.AppView = Backbone.View.extend({
-      
-        //tagName: "nav",
-        //className: "mr_peepers",
-        
-        initialize: function() {
-            
-            //Sets.bind('add',   this.addJot, this);
-            //Sets.bind('reset', this.addAllJots, this);
-            //Sets.bind('all',   this.render, this);
-            
-            Sets.fetch();
-        },
-        
-        render: function() {
-          console.log(this.el);
-          this.$el = $(this.el);
-        }
-    });
-    
-    window.App = new AppView;
 });
