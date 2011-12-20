36ae9d43e799a831ed85321419b5e5ed15dc8327
Andrew Gruner <andrew@divineflame.com>
2011-12-20 00:07:43 -0800

    getting ckick events sort of working

--- src/BibleWorm/MobileBundle/Resources/public/js/readingplan.js
+++ src/BibleWorm/MobileBundle/Resources/public/js/readingplan.js
@@ -1,16 +1,34 @@
-$(function(){
+$(function() {
 
-    var $readings = $(.ui-checkbox);
+    var $readings = $('.ui-checkbox');
     
     $readings.click(function() {
         var $reading = $(this),
             $parent = $reading.parents('.ui-collapsible'),
-            $siblings = $reading.siblings();
+            $siblings = $reading.siblings(),
+            ref = $reading.find('.ui-btn-text').text();
         
+
         // open reading
         
         // mark set as read if all siblings are checked
         
+        // this input isn't checked yet, it gets checked
+        // as a result of this click, so we count it as checked
+        var checked = !$reading.find('input').is(':checked');
+        
+        if (checked & $parent.find(':checked').length === 2) {
+          $parent.find('.ui-btn').attr('data-theme', 'b');
+        } else {
+          $parent.find('.ui-btn').attr('data-theme', 'c');
+        }
+        
         // save state
     });
 });
+
+// $('div[data-role="content"]').live('pageinit', function(event) {
+//     var $reading = $(this);
+//     
+//     alert( 'This page was just enhanced by jQuery Mobile!' );
+// });
